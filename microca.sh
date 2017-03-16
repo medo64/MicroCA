3a0d68e6b1649266fb3b357c9f6e427fd54a00fa
Josip Medved <jmedved@jmedved.com>
2017-03-15 23:38:49 -0700

    Issuer is added to Authority Key Identifier only if Subject Key Identifier is missing.

--- microca.sh
+++ microca.sh
@@ -262,12 +262,10 @@ trap ctrl_c INT
 cat $OPENSSL_CONFIG > $TEMP_FILE_EXTENSIONS
 echo -e "\n[myext]" >> $TEMP_FILE_EXTENSIONS
 echo "subjectKeyIdentifier=hash" >> $TEMP_FILE_EXTENSIONS
-if ! (( $CA_CREATE_ROOT )) && ! (( CERTIFICATE_SELF )); then
-    if (( $CA_CREATE )); then
-        echo "authorityKeyIdentifier=keyid:always,issuer:always" >> $TEMP_FILE_EXTENSIONS
-    else
-        echo "authorityKeyIdentifier=keyid,issuer:always" >> $TEMP_FILE_EXTENSIONS
-    fi
+if (( $CA_CREATE )) || (( CERTIFICATE_SELF )); then
+    echo "authorityKeyIdentifier=keyid:always,issuer" >> $TEMP_FILE_EXTENSIONS
+else
+    echo "authorityKeyIdentifier=keyid,issuer" >> $TEMP_FILE_EXTENSIONS
 fi
 if (( $CA_CREATE )); then
     echo "basicConstraints=CA:true" >> $TEMP_FILE_EXTENSIONS
